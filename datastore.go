0d5be64bee8c79fa42d11ee63a68e27f684220c4
godcong <jumbycc@163.com>
2019-12-22 00:56:10 +0800

    update tikv

--- datastore.go
+++ datastore.go
@@ -1,17 +1,19 @@
 package tikv
 
 import (
+	"context"
+	"errors"
 	ds "github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/query"
 	logger "github.com/ipfs/go-log"
-	"github.com/pingcap/tidb/config"
-	"github.com/pingcap/tidb/store/tikv"
+	"github.com/tikv/client-go/config"
+	"github.com/tikv/client-go/txnkv"
 	"sync"
 	"time"
 )
 
 type Datastore struct {
-	cli       *tikv.RawKVClient
+	cli       *txnkv.Client
 	closeLk   sync.RWMutex
 	closed    bool
 	closeOnce sync.Once
@@ -22,6 +24,46 @@ type Datastore struct {
 	gcInterval     time.Duration
 
 	syncWrites bool
+}
+
+// Implements the datastore.Txn interface, enabling transaction support for
+// the badger Datastore.
+type txn struct {
+	ds  *Datastore
+	txn *txnkv.Transaction
+
+	// Whether this transaction has been implicitly created as a result of a direct Datastore
+	// method invocation.
+	implicit bool
+}
+
+// Options are the badger datastore options, reexported here for convenience.
+type Options struct {
+	config.Config
+}
+
+var ErrClosed = errors.New("datastore closed")
+
+var _ ds.Datastore = (*Datastore)(nil)
+var _ ds.TxnDatastore = (*Datastore)(nil)
+var _ ds.TTLDatastore = (*Datastore)(nil)
+var _ ds.GCDatastore = (*Datastore)(nil)
+
+var log = logger.Logger("tikv")
+
+func NewDatastore(addr []string, options *Options) (*Datastore, error) {
+	kv, err := txnkv.NewClient(context.TODO(), []string{"127.0.0.1:2379"}, options.Config)
+	//kv, err := rawkv.NewClient(context.TODO(), []string{"127.0.0.1:2379"}, options.Config)
+	if err != nil {
+		return nil, err
+	}
+
+	ds := &Datastore{
+		cli:     kv,
+		closing: make(chan struct{}),
+	}
+
+	return ds, nil
 }
 
 func (d *Datastore) PutWithTTL(key ds.Key, value []byte, ttl time.Duration) error {
@@ -57,7 +99,20 @@ func (d *Datastore) Query(q query.Query) (query.Results, error) {
 }
 
 func (d *Datastore) Put(key ds.Key, value []byte) error {
-	panic("ds.Datastore")
+	d.closeLk.RLock()
+	defer d.closeLk.RUnlock()
+	if d.closed {
+		return ErrClosed
+	}
+
+	txn := d.newImplicitTransaction(false)
+	defer txn.discard()
+
+	if err := txn.put(key, value); err != nil {
+		return err
+	}
+
+	return txn.commit()
 }
 
 func (d *Datastore) Delete(key ds.Key) error {
@@ -76,28 +131,24 @@ func (d *Datastore) CollectGarbage() error {
 	panic("ds.Datastore")
 }
 
-// Options are the badger datastore options, reexported here for convenience.
-type Options struct {
-	config.Security
+// newImplicitTransaction creates a transaction marked as 'implicit'.
+// Implicit transactions are created by Datastore methods performing single operations.
+func (d *Datastore) newImplicitTransaction(readOnly bool) *txn {
+	t, err := d.cli.Begin(context.TODO())
+	if err != nil {
+		return nil
+	}
+	return &txn{d, t, true}
 }
 
-var _ ds.Datastore = (*Datastore)(nil)
-var _ ds.TxnDatastore = (*Datastore)(nil)
-var _ ds.TTLDatastore = (*Datastore)(nil)
-var _ ds.GCDatastore = (*Datastore)(nil)
+func (t *txn) put(key ds.Key, bytes []byte) error {
+	return t.txn.Set(key.Bytes(), bytes)
+}
 
-var log = logger.Logger("tikv")
+func (t *txn) commit() error {
+	return t.txn.Commit(context.Background())
+}
 
-func NewDatastore(addr []string, options *Options) (*Datastore, error) {
-	kv, err := tikv.NewRawKVClient(addr, options.Security)
-	if err != nil {
-		return nil, err
-	}
+func (t *txn) discard() {
 
-	ds := &Datastore{
-		cli:     kv,
-		closing: make(chan struct{}),
-	}
-
-	return ds, nil
 }
