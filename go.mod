703dd224a4a1238239fdeedac237dc9803bed854
godcong <jumbycc@163.com>
2020-01-05 02:05:51 +0800

    update tikv

--- go.mod
+++ go.mod
@@ -4,18 +4,21 @@ go 1.13
 
 require (
 	github.com/coreos/etcd v3.3.18+incompatible // indirect
-	github.com/google/btree v1.0.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.1.0 // indirect
 	github.com/ipfs/go-datastore v0.3.1
+	github.com/ipfs/go-ipfs v0.4.22
 	github.com/ipfs/go-log v1.0.0
-	github.com/jbenet/goprocess v0.0.0-20160826012719-b497e2f366b8
+	github.com/jbenet/goprocess v0.1.3
 	github.com/pingcap/check v0.0.0-20191216031241-8a5a85928f12 // indirect
 	github.com/pingcap/goleveldb v0.0.0-20191031114657-7683883cfb36 // indirect
 	github.com/pingcap/kvproto v0.0.0-20191217072959-393e6c0fd4b7 // indirect
 	github.com/prometheus/client_golang v1.2.1 // indirect
 	github.com/tikv/client-go v0.0.0-20191220031903-b252c0420df4
 	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
-	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/grpc v1.26.0 // indirect
 	sigs.k8s.io/yaml v1.1.0 // indirect
 )
+
+replace github.com/go-critic/go-critic v0.0.0-20181204210945-ee9bf5809ead => github.com/go-critic/go-critic v0.4.0
+
+replace github.com/golangci/golangci-lint v1.16.1-0.20190425135923-692dacb773b7 => github.com/golangci/golangci-lint v1.21.0
---
+++ plugin/tikvds.go
@@ -0,0 +1,67 @@
+package plugin
+
+import (
+	"fmt"
+	"github.com/tikv/client-go/config"
+
+	tikv "github.com/godcong/go-ds-tikv"
+	"github.com/ipfs/go-ipfs/plugin"
+	"github.com/ipfs/go-ipfs/repo"
+	"github.com/ipfs/go-ipfs/repo/fsrepo"
+)
+
+var Plugins = []plugin.Plugin{
+	&TiKVPlugin{},
+}
+
+type TiKVPlugin struct{}
+
+func (t TiKVPlugin) Name() string {
+	return "s3-datastore-plugin"
+}
+
+func (t TiKVPlugin) Version() string {
+	return "0.0.1"
+}
+
+func (t TiKVPlugin) Init() error {
+	return nil
+}
+
+func (t TiKVPlugin) DatastoreTypeName() string {
+	return "s3ds"
+}
+
+func (t TiKVPlugin) DatastoreConfigParser() fsrepo.ConfigFromMap {
+	return func(m map[string]interface{}) (fsrepo.DatastoreConfig, error) {
+		var workers int
+		if v, ok := m["workers"]; ok {
+			workersf, ok := v.(float64)
+			workers = int(workersf)
+			switch {
+			case !ok:
+				return nil, fmt.Errorf("s3ds: workers not a number")
+			case workers <= 0:
+				return nil, fmt.Errorf("s3ds: workers <= 0: %f", workersf)
+			case float64(workers) != workersf:
+				return nil, fmt.Errorf("s3ds: workers is not an integer: %f", workersf)
+			}
+		}
+
+		return &TiKVConfig{
+			cfg: config.Default(),
+		}, nil
+	}
+}
+
+type TiKVConfig struct {
+	cfg config.Config
+}
+
+func (t *TiKVConfig) DiskSpec() fsrepo.DiskSpec {
+	return fsrepo.DiskSpec{}
+}
+
+func (t *TiKVConfig) Create(path string) (repo.Datastore, error) {
+	return tikv.NewDatastore([]string{path}, &tikv.Options{Config: t.cfg})
+}
